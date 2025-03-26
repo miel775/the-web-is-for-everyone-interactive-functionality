b8a25401a3b4c91cad10ae0ccb51ea4584d51e88
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-03-26 12:04:24 +0100

    States toegepast
    Alle states die ik wil toepassen staat er.
    
    #3

--- public/script/opening-comment.js
+++ public/script/opening-comment.js
@@ -1,11 +1,22 @@
 const formElements = document.querySelectorAll('.hide');
 const textArea = document.querySelector('.text-area');
+const typeArea = document.querySelector('.type');
 
-textArea.addEventListener('click', function() {
-    formElements.forEach(element => element.classList.remove('hide'));
-    formElements.forEach(element => element.classList.add('opening-comment'))
+textArea.addEventListener('click', function () {
+    formElements.forEach(element => {
+        element.classList.remove('hide');
+        element.classList.add('opening-comment');
+    });
 
-    textArea.classList.add('opening-comment')
+    textArea.classList.add('opening-comment');
 });
 
 
+document.addEventListener('click', function (event) {
+    if (!typeArea.contains(event.target) && !textArea.contains(event.target)) {
+        formElements.forEach(element => element.classList.remove('opening-comment'));
+        textArea.classList.remove('opening-comment');
+    }
+});
+
+const animationTiming = 1000;

3a4f82ea0738fd4816823584e87336cbdcbea4ec
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-03-31 11:23:21 +0200

    If empty situatie gemaakt
    Met if heb ik een situatie gemaakt die checkt of de comment sectie leeg is.
    
    #3

--- public/script/uistates.js
+++ public/script/uistates.js
@@ -1,6 +1,7 @@
 const loadingState = document.getElementById('loading-state');
 const errorState = document.getElementById('error-state');
 const successState = document.getElementById('success-state');
+const emptyState = document.getElementById('empty');
 const progressBar = document.getElementById('file');
 // dit zijn alle states met de progressbar
 
@@ -24,3 +25,8 @@ function showError() {
     successState.style.display = 'none';
 };
 
+if ('fetch' in window)
+
+// wanneer de commentsectie leeg is, dan laat hij een bericht zien dat de comment sectie
+// als de commentsectie leeg is, dan laat hij de emptystate zien
+// wanneer er wel een comment in de sectie zit (else) dan wordt staat emptystate uit
